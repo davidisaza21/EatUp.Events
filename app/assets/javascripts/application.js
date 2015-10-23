68189a68f284242c37be49ec2206cb140420f84f
Steve Toro <steve.toro@gmail.com>
2015-10-22 22:19:10 -0400

    jquery validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require mailgun_validator
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require mailgun_validator.js
