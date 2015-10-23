d7394d29e85088b0b8bc67dee7c0150a22f611ab
David Isaza <Davidisaza21@gmail.com>
2015-10-23 19:58:49 -0400

    landing page final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
-  def new
+  
+	def new
   	@user = User.new
   end
   def user_params
 		params.require(:user).permit(:email, :location, :created_at, :updated_at)
 	end
 
+
 	def create
 	  @user = User.new(user_params)
-	  if @user.save
-# If save succeeds, redirect to the list action
-			flash[:success] = "user was created successfully"
+	  if @user.save		
 		else
 # If save fails, redisplay the form so user can fix problems
 			redirect_to root_path
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module App
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.precompile += %w( mailgun_validator.js )
+
   end
 end
